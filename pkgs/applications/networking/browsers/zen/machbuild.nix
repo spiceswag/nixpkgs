46953d1f66c241d5ca41365ca78ca121a5738f12
spicesw <spicesw@proton.me>
2026-06-21 12:18:04 +0300

    add libclang dependency for release profile

--- pkgs/applications/networking/browsers/zen/machbuild.nix
+++ pkgs/applications/networking/browsers/zen/machbuild.nix
@@ -1,12 +1,19 @@
-{ buildMozillaMach, callPackage }:
+{
+  buildMozillaMach,
+  callPackage,
+  libclang,
+}:
 let
   worktree = callPackage ./prepare.nix { };
 
   first = buildMozillaMach {
     pname = "zen-browser-build";
     version = worktree.firefoxVersion;
+
     src = worktree;
     meta.maxSilent = 14400; # 4h, double the default of 7200s (c.f. #129212, #129115)
+
+    extraNativeBuildInputs = [ libclang.dev ];
   };
 
   second = first.override { enableOfficialBranding = false; };
