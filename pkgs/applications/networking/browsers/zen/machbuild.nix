bcdec094c6c598871389f2e922a11f88b85e21be
spicesw <spicesw@proton.me>
2026-06-28 23:24:25 +0300

    set correct build flags

--- pkgs/applications/networking/browsers/zen/machbuild.nix
+++ pkgs/applications/networking/browsers/zen/machbuild.nix
@@ -1,19 +1,12 @@
-{
-  buildMozillaMach,
-  callPackage,
-  libclang,
-}:
+{ buildMozillaMach, callPackage }:
 let
   worktree = callPackage ./prepare.nix { };
 
   first = buildMozillaMach {
     pname = "zen-browser-build";
     version = worktree.firefoxVersion;
-
     src = worktree;
     meta.maxSilent = 14400; # 4h, double the default of 7200s (c.f. #129212, #129115)
-
-    extraNativeBuildInputs = [ libclang.dev ];
   };
 
   second = first.override { enableOfficialBranding = false; };
@@ -21,6 +14,16 @@ let
   third = second.overrideAttrs (
     final: prev: {
       ZEN_RELEASE = 1;
+      SURFER_COMPAT =
+        let
+          host = final.finalPackage.stdenv.hostPlatform;
+        in
+        if host.isLinux && host.isx86_64 then
+          "x86_64"
+        else if host.isLinux && host.isAarch64 then
+          "aarch64"
+        else
+          "";
 
       prePatch = ''
         pushd ./engine
